b11b755044dc9ed1d7dc768204fe8b9559348c32
muhsin sutanto <sutantomuhsin@gmail.com>
2023-02-18 22:39:01 +0700

    add palindrome

--- palindrome.js
+++ palindrome.js
@@ -1,8 +1,20 @@
 const isPalinddrom = (param) => {
-  const str = param
-  const reversStr = str.split('').reverse().join('')
+   const str = param;
+   const reversStr = str.split("").reverse().join("");
 
-  return reversStr === str
+   return reversStr === str;
+};
+
+function isPalindrome(str) {
+   if (str.length === 1 || !str.length) return false;
+   let revStr = [];
+   for (let i = str.length; i >= 0; i--) {
+      revStr.push(str[i]);
+   }
+
+   return revStr.join("") === str;
 }
 
-console.log(isPalinddrom('civic'))
+// console.log(isPalinddrom('civic'))
+
+console.log(isPalindrome("civic"));
